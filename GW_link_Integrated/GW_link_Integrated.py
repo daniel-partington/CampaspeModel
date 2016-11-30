a7f6e58ab486a197e6a7696484c99fc5920c4a90
Takuya Iwanaga <iwanaga.takuya@anu.edu.au>
2016-11-30 16:23:55 +1100

    Cleared whitespace

--- GW_link_Integrated/GW_link_Integrated.py
+++ GW_link_Integrated/GW_link_Integrated.py
@@ -16,20 +16,20 @@ from HydroModelBuilder.ModelInterface.flopyInterface import flopyInterface
 # Configuration Loader
 from HydroModelBuilder.Utilities.Config.ConfigLoader import CONFIG
 
-#TODO: Set the stream gauges, ecology bores, policy bores at the start in some
+# TODO: Set the stream gauges, ecology bores, policy bores at the start in some
 # other class or in here but so they are available in the run function.
 
-def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=None, 
-        rainfall_irrigation=None, pumping=None):
 
+def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=None,
+        rainfall_irrigation=None, pumping=None):
     """
     GW Model Runner
-    
+
     :param riv_stages: np rec array fo gauge number and stage
-  
-    
+
+
     """
- 
+
     def loadObj(data_folder, model_name, filename):
         """
         Interface to Model Manager object loader.
@@ -55,14 +55,12 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
 
     MM = GWModelManager()
     MM.load_GW_model(os.path.join(model_folder, r"01_steady_state_packaged.pkl"))
-    
-    
+
     name = MM.GW_build.keys()[0]
 
     # Load in the new parameters based on parameters.txt or dictionary of new parameters
     if param_file:
         MM.GW_build[name].updateModelParameters(os.path.join(data_folder, 'parameters.txt'))
-    
 
     # This needs to be automatically generated from with the map_raster2mesh routine ...
 #    zone_map = {1: 'qa', 2: 'utb', 3: 'utqa', 4: 'utam', 5: 'utaf', 6: 'lta', 7: 'bse'}
@@ -81,7 +79,7 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
         if MM.GW_build[name].model_mesh3D[1][0][row][col] == -1:
             continue
 
-    #TODO: Update stage data with interpolated values based on riv_stages passed in to function
+    # TODO: Update stage data with interpolated values based on riv_stages passed in to function
 
         stage = MM.GW_build[name].model_mesh3D[0][0][row][col] - 0.01
         bed = MM.GW_build[name].model_mesh3D[0][0][row][col] - 0.1 - \
@@ -96,7 +94,7 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     # River', 'river', bc_static=True)
 
     MM.GW_build[name].boundaries.assign_boundary_array('Campaspe River', riv)
-    
+
     mapped_river = loadObj(data_folder, name, r"River_Murray_model.shp_mapped.pkl")
 
     simple_river = []
@@ -127,11 +125,11 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     # Adjust rainfall to recharge using 10% magic number
     interp_rain = np.copy(MM.GW_build[name].boundaries.bc['Rainfall']['bc_array'])
 
-    #TODO: Replace interp_rain with input from farm model, i.e. rainfall_irrigation
-    
+    # TODO: Replace interp_rain with input from farm model, i.e. rainfall_irrigation
+
     for i in [1, 2, 3, 7]:
         interp_rain[MM.GW_build[name].model_mesh3D[1][0] == i] = interp_rain[MM.GW_build[name].model_mesh3D[
-            1][0] == i] * 0.1 #MM.GW_build[name].parameters.param['rch_red_' + zone_map[i]]['PARVAL1']
+            1][0] == i] * 0.1  # MM.GW_build[name].parameters.param['rch_red_' + zone_map[i]]['PARVAL1']
 
     for i in [4, 5, 6, ]:
         interp_rain[MM.GW_build[name].model_mesh3D[1][0] == i] = interp_rain[
@@ -141,7 +139,6 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     rch[0] = interp_rain
 
     MM.GW_build[name].boundaries.assign_boundary_array('Rain_reduced', rch)
-
 
     print "************************************************************************"
     print " Updating Murray River GHB boundary"
@@ -165,7 +162,6 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     ghb = {}
     ghb[0] = MurrayGHB
 
-
     print "************************************************************************"
     print " Updating GHB boundary"
 
@@ -174,9 +170,9 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     print "************************************************************************"
     print " Set initial head "
 
-    #TODO: Update head based on last iteration rather than initial head
-    fname="initial"
-    headobj = bf.HeadFile(os.path.join(data_folder, fname) +'.hds')
+    # TODO: Update head based on last iteration rather than initial head
+    fname = "initial"
+    headobj = bf.HeadFile(os.path.join(data_folder, fname) + '.hds')
     times = headobj.get_times()
     head = headobj.get_data(totim=times[-1])
 
@@ -192,11 +188,11 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     modflow_model = flopyInterface.ModflowModel(MM.GW_build[name], data_folder=data_folder)
 
     # Override temporal aspects of model build:
-    modflow_model.nper = 1 # This is the number of stress periods which is set to 1 here
-    modflow_model.perlen = 1 # This is the period of time which is set to 1 day here 
-    modflow_model.nstp = 1 # This is the number of sub-steps to do in each stress period
-    modflow_model.steady = False # This is to tell FloPy that is a transient model
-    
+    modflow_model.nper = 1  # This is the number of stress periods which is set to 1 here
+    modflow_model.perlen = 1  # This is the period of time which is set to 1 day here
+    modflow_model.nstp = 1  # This is the number of sub-steps to do in each stress period
+    modflow_model.steady = False  # This is to tell FloPy that is a transient model
+
     modflow_model.executable = mf_exe_folder
 
     modflow_model.buildMODFLOW()
@@ -210,13 +206,12 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     """
 
     sw_stream_gauges = [406214, 406219, 406201, 406224, 406218, 406202, 406265]
-    swgw_exchanges = np.recarray((1,), dtype=[(str(gauge), np.float) for gauge in sw_stream_gauges])    
+    swgw_exchanges = np.recarray((1,), dtype=[(str(gauge), np.float) for gauge in sw_stream_gauges])
 
-    #TODO: Generate segment subsets of river nodes list for each gauging station
+    # TODO: Generate segment subsets of river nodes list for each gauging station
     # and use the following function already written to grab the flux for the
     # chosen nodes
     #riv_exch = modflow_model.getRiverFlux('Campaspe River')
-
 
     """
     Average depth to GW table:
@@ -224,9 +219,10 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     """
 
     farm_zones = [1]
-    avg_depth_to_gw = np.recarray((1,), dtype=[(str(farm_zone), np.float) for farm_zone in farm_zones])
-    
-    #TODO: Get average depth to GW using:
+    avg_depth_to_gw = np.recarray(
+        (1,), dtype=[(str(farm_zone), np.float) for farm_zone in farm_zones])
+
+    # TODO: Get average depth to GW using:
     # 1. Active cells heads in the first layer of the model
     # 2. Surface elevation in at the top of those active cells:
     # 3. Average the difference between the two arrays
@@ -238,18 +234,18 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
     Corresponding GW bores nearest to:      83003,  89586,  82999,  5662,   44828
         
     """
-    
+
     ecol_depth_to_gw_bores = ['83003', '89586', '82999', '5662', '44828']
-    ecol_depth_to_gw = np.recarray((1,), dtype=[(bore, np.float) for bore in ecol_depth_to_gw_bores])    
+    ecol_depth_to_gw = np.recarray((1,), dtype=[(bore, np.float)
+                                                for bore in ecol_depth_to_gw_bores])
     # to set
     for ecol_bore in ecol_depth_to_gw_bores:
         ecol_depth_to_gw[ecol_bore] = np.random.rand()
     # end for
-    
-    #TODO: Check that all of the wells listed were mapped to the model mesh and
+
+    # TODO: Check that all of the wells listed were mapped to the model mesh and
     # are available for inspection
-    
-    
+
     """
     Trigger heads of importance for policy
 
@@ -264,20 +260,19 @@ def run(model_folder, data_folder, mf_exe_folder, param_file=None, riv_stages=No
        Murray. So can we have the gradient of flow as an indicator in the 
        integrated model as well (if it isn’t already)?
     """
-    
+
     trigger_head_bores = ['79234', '62589']
     trigger_heads = np.recarray((1, ), dtype=[(bore, np.float) for bore in trigger_head_bores])
     # to set
     for trigger_bore in trigger_head_bores:
         trigger_heads[trigger_bore] = np.random.rand()
     # end for
-   
-    #TODO: Check that all of the wells listed were mapped to the model mesh and
+
+    # TODO: Check that all of the wells listed were mapped to the model mesh and
     # are available for inspection
 
-    
-    return swgw_exchanges, avg_depth_to_gw, ecol_depth_to_gw, trigger_heads 
-    
+    return swgw_exchanges, avg_depth_to_gw, ecol_depth_to_gw, trigger_heads
+
 if __name__ == "__main__":
     args = sys.argv
     if len(args) > 1:
