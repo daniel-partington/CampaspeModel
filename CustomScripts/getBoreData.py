27b9f0fa3ed164e1f881a8a49778e3954d6a259f
daniel-partington <dpartington1982@gmail.com>
2017-04-24 17:51:30 +0930

    Mod config file and also changed filtering in gerBoreData

--- CustomScripts/getBoreData.py
+++ CustomScripts/getBoreData.py
@@ -200,7 +200,7 @@ def getBoreData(get='transient', base_path=None):
 
     # Filter bores by those with only one construction record as multiscreened
     # wells are ambiguous with respect to observations in NGIS database
-    df_bores_clear = df_bores_clear[df_bores_clear['result'] < 3]
+    # df_bores_clear = df_bores_clear[df_bores_clear['result'] < 3]
 
     print 'Total number of bores with levels and screen info non-ambiguous: ', df_bores_clear.shape[0]
 
@@ -232,6 +232,8 @@ def getBoreData(get='transient', base_path=None):
     # df_HydrogeologicUnit, df_level, df_salinity
 
     # , df_ConstructionLog_VIC #, df_HydrogeologicUnit, df_level, df_salinity
+    
+
     return df_level_ordered, df_bores_clear
 
     #filter_wells = getWells(df_level, wellslist)
@@ -242,5 +244,5 @@ def getBoreData(get='transient', base_path=None):
     #ax.plot(title="Bore @" + wellslist[0])
 
 if __name__ == "__main__":
-    df_level, df_bores = getBoreData(get='transient')
+    df_level, df_bores = getBoreData() #get='transient')
     # getBoreDataGMW()
