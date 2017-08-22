32f0c1b93eac05526ea6daab845c64cfb4d3fe50
Takuya Iwanaga <takuyai@gmail.com>
2017-08-22 12:34:57 +1000

    added missing dependency

--- docker_build/conda.recipe/setup.py
+++ docker_build/conda.recipe/setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'https://github.com/daniel-partington/CampaspeModel/archive/master.zip',
     'author_email': 'dpartington1982@gmail.com',
     'version': '0.1',
-    'install_requires':['flopy', 'numpy', 'pandas', 'gdal'],
+    'install_requires':['flopy', 'numpy', 'pandas', 'gdal', 'matplotlib'],
     'packages': ['CampaspeModel', 'CampaspeModel.GW_link_Integrated'],
     'scripts': [],
     'name': 'CampaspeModel'
