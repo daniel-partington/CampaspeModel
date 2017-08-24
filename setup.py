f7804773ae5e9d52d188e1e7b526d6a7b25f23ef
Takuya Iwanaga <takuyai@gmail.com>
2017-08-24 11:14:13 +1000

    Autofind packages in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
+    import re
+    import os
+
+    def find_packages(path='.'):
+        ret = []
+        for root, dirs, files in os.walk(path):
+            if '__init__.py' in files:
+                ret.append(re.sub('^[^A-z0-9_]+', '', root.replace('/', '.')))
+        return ret
+    # End find_packages()
+# End try
 
 config = {
     'description': 'CampaspeModel',
@@ -10,10 +21,10 @@ config = {
     'download_url': 'https://github.com/daniel-partington/CampaspeModel/archive/master.zip',
     'author_email': 'dpartington1982@gmail.com',
     'version': '0.1',
-    'install_requires':['flopy', 'numpy', 'pandas', 'gdal', 'matplotlib'],
-    'packages': ['CampaspeModel', 'CampaspeModel.GW_link_Integrated'],
+    'install_requires': ['flopy', 'numpy', 'pandas', 'gdal', 'matplotlib'],
+    'packages': find_packages(),
     'scripts': [],
     'name': 'CampaspeModel'
-    }
-	
-setup(**config)	
+}
+
+setup(**config)
