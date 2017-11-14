71e1fb29469b8a4c8f0fb0247b733a225986644d
daniel-partington <dpartington1982@gmail.com>
2017-11-14 12:17:24 +1030

    Some tidying up of processWeatherStations.py

--- CampaspeModel/CustomScripts/processWeatherStations.py
+++ CampaspeModel/CustomScripts/processWeatherStations.py
@@ -8,6 +8,8 @@ def processWeatherStations(weather_stations, path='', frequency='A', \
                            plot_monthly_pattern=False, \
                            plot_yearly_rainfall=False):
     
+    assert frequency in ['A', 'M'], "Frequency must be either: 'A' or 'M'"
+    
     weather_station_details = {}
     weather_dfs = {}
     
@@ -37,46 +39,6 @@ def processWeatherStations(weather_stations, path='', frequency='A', \
                                            skipinitialspace=True, 
                                            usecols=[0,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
 
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# @@@ This little chuck below was used for processing BOM data for Eppalock  @@
-# @@@ but it has been updated with SILO data ... going forward would be good @@
-# @@@ to make general SILO and BOM functions for processing and import them  @@
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    
-#    #  Lake Eppalock is in a different format so need something else to process
-#    other_station = 'Eppalock Reservoir'
-#    other_path = path + r"Rainfall at Eppalock\\"
-#    with open(other_path + 'IDCJAC0009_081083_1800_Note.txt', 'r') as f:
-#        text = f.read()
-#        
-#        station_number = re.search('Bureau of Meteorology station number: (\S+)', text).group(1)
-#    
-#        # Get station Lat Long which corresponds to GDA94:
-#     
-#        station_lat = re.search('Latitude \(decimal degrees, south negative\): (\S+)', text).group(1)
-#        station_long = re.search('Longitude \(decimal degrees, east positive\): (\S+)', text).group(1)
-#        station_latlong = station_lat + " " + station_long
-#    
-#        # Get elevation of station:
-#        station_elev = re.search('Height of station above mean sea level \(metres\): (\S+)', text).group(1)
-#    
-#        weather_station_details[other_station] = [station_number, station_latlong , station_elev]
-#        
-#        #Read in time series data:
-#        weather_dfs[other_station] = pd.read_csv(other_path + 'IDCJAC0009_081083_1800_Data.csv', 
-#                                               index_col=0, 
-#                                               skiprows=[41], 
-#                                               parse_dates=[[2,3,4]], #True, 
-#                                               #index_col = [[2,3,4]],
-#                                               infer_datetime_format=True, 
-#                                               comment='"', 
-#                                               skipinitialspace=True 
-#                                               )
-
-    # Get info:
-    
-    # Look at long term mean values for each
-    
     def cm2inch(*tupl):
         inch = 2.54
         if isinstance(tupl[0], tuple):
@@ -84,21 +46,25 @@ def processWeatherStations(weather_stations, path='', frequency='A', \
         else:
             return tuple(i/inch for i in tupl)
     
-   
-    annual_weather_df = pd.DataFrame()
-    for station in weather_stations:
-        annual_weather_df[station]= weather_dfs[station]['Rain'].resample("A").sum()
-        annual_weather_df[station + '_ET']= weather_dfs[station]['Evap'].resample("A").sum()
 
-#    annual_weather_df[other_station]= weather_dfs[other_station]['Rainfall amount (millimetres)'].resample("A").sum()    
-    
-    monthly_weather_df = pd.DataFrame() 
-    for station in weather_stations:
-        monthly_weather_df[station]= weather_dfs[station]['Rain'].resample("M").mean()
-        monthly_weather_df[station + '_ET']= weather_dfs[station]['Evap'].resample("M").mean()
+    def get_rain_and_ET_from_df(df, stations, freq, how='sum'):
+        new_df = pd.DataFrame()
+        for station in stations:
+            if how == 'mean':
+                new_df.loc[:, station] = df[station]['Rain'].resample(freq).mean()
+                new_df.loc[:, station + '_ET'] = df[station]['Evap'].resample(freq).mean()
+            elif how == 'sum':
+                new_df.loc[:, station] = df[station]['Rain'].resample(freq).sum()
+                new_df.loc[:, station + '_ET'] = df[station]['Evap'].resample(freq).sum()
+            # end if
+        #end for
+        return new_df
+        
+    annual_weather_df = get_rain_and_ET_from_df(weather_dfs, weather_stations,
+                                                'A', how='sum')
+    monthly_weather_df = get_rain_and_ET_from_df(weather_dfs, weather_stations,
+                                                'M', how='mean') 
 
-#    monthly_weather_df[other_station]= weather_dfs[other_station]['Rainfall amount (millimetres)'].resample("M").mean()    
-    
     if plot_yearly_rainfall:
         plt.figure(figsize=cm2inch(18,8))
         plt.ylabel("Annual Rainfall [mm]")
@@ -118,7 +84,7 @@ def processWeatherStations(weather_stations, path='', frequency='A', \
 
         annual_weather_df.plot(kind='box')
         plt.ylabel("Annual Rainfall [mm]")
-
+        
     if plot_monthly_pattern:
         Months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         month_avg = pd.groupby(monthly_weather_df,by=[monthly_weather_df.index.month]).mean()
@@ -132,6 +98,7 @@ def processWeatherStations(weather_stations, path='', frequency='A', \
         plt.legend(bbox_to_anchor=(0, 1), loc='upper left', ncol=1)
 
     if frequency == 'A':
+        # Keeping this as is for now but should not calculate mean here 
         return annual_weather_df.mean()
     if frequency == 'M':
         return monthly_weather_df
